8504d423b57e495bb6eb27fd2db97e786fb0142f
Anthony Bishopric <anthony.bishopric@snorkel.ai>
2021-02-25 07:31:42 -0800

    print time to connect

--- main.go
+++ main.go
@@ -8,9 +8,13 @@ import (
 )
 
 func main() {
-   _, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]), time.Duration(5 * time.Second))
+  start := time.Now()
+  conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]), time.Duration(5 * time.Second))
    if err != nil {
 	   os.Stderr.WriteString(fmt.Sprintf("failed to connect: %s", err.Error()))
 	   os.Exit(1)
 	}
+  defer conn.Close()
+  elapsed := time.Since(start)
+  fmt.Printf("Connection to %s:%s took %s\n", os.Args[1], os.Args[2], elapsed)
 }
